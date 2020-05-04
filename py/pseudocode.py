74f121ef8de259e0907b3779c2c81866d1aced65
gabastil <glenn.abastillas@gmail.com>
2020-05-04 17:39:18 -0400

    Updated index template and pseudocode files

--- py/pseudocode.py
+++ py/pseudocode.py
@@ -1,10 +1,21 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, url_for, redirect, render_template
 
 app = Flask(__name__)
 
+test_data = 'bob joe jack jane jill jess'.split()
+
 @app.route("/")
 def home():
-	return render_template("../index.html", data='this is a test'.split())
+	return render_template("index.html", data=test_data, length=len(test_data))
+
+@app.route("/<name>")
+def user(name):
+    data = name.split()
+    return render_template("index.html", data=data, length=len(data))
+
+@app.route("/admin/<name>")
+def admin(name):
+    return redirect(url_for("user", name=name))
 
 if __name__ == "__main__":
 	app.run()
